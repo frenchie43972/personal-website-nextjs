02be52f37443010d7734b1fe6ef349acddc27d92
Kris French <frenchie43972@gmail.com>
2023-12-15 18:56:40 +0900

    Working the home page

--- personal-website-nextjs/src/app/page.tsx
+++ personal-website-nextjs/src/app/page.tsx
@@ -1,6 +1,23 @@
+import Image from "next/image"
+import profilePic from '/public/profile_pic.jpg';
 
 export default function Home() {
   return (
-    <div>Hi There!</div>
+    <div className="container mx-auto p-8 flex flex-col items-center justify-center">
+      <div className="w-64 h-64 relative rounded-full overflow-hidden mb-10">
+        <Image 
+          alt="Picture of a very handsome dude" 
+          src={profilePic}
+          objectFit="cover"
+          layout="fill"
+        />
+      </div>
+      <div>
+        <p className="text-4xl mb-4"> Hello, my name is Kris French.</p>
+        <p className="text-2xl">Welcome, everything is fine!</p>
+      </div>
+    </div>
+    
+    
   )
 }
--- personal-website-nextjs/src/components/header.tsx
+++ personal-website-nextjs/src/components/header.tsx
@@ -2,22 +2,18 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <nav className="flex items-center justify-between flex-wrap p-6 bg-slate-200 shadow-md">
-            <div className="flex items-center flex-shrink-0 mr-6">
-                <Link href="/" className="text-2xl tracking-tight">Home</Link>
-            </div>
-            <div className="w-full block flex-grow lg:items-end text-lg lg:w-auto">
-                <div className="lg:flex-grow">
-                    <Link href="/about">About Me</Link>
+
+        <div className='w-full bg-slate-200 shadow-md z-10'>
+            <nav className='container relative flex flex-wrap items-center justify-between mx-auto p-8'>
+                <Link href='/' className='font-bold text-3xl'>Home</Link>
+                <div className='space-x-8 text-xl'>
+                    <Link href='/about'>About Me</Link>
+                    <Link href='/portfolio'>Portfolio</Link>
+                    <Link href='/contact'>Contact Me</Link>
                 </div>
-                <div className=" lg:flex-grow">
-                    <Link href="/portfolio">Portfolio</Link>
-                </div>
-                <div className="lg:flex-grow">
-                    <Link href="/contact">Contact Me</Link>
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
+        
     );
 };
 
@@ -54,3 +50,20 @@ export default function Header() {
                     </div>
                 </div>
             </div> */}
+
+        //     <nav className="flex items-center justify-between flex-wrap p-6 bg-slate-200 shadow-md">
+        //     <div className="flex items-center flex-shrink-0 mr-6">
+        //         <Link href="/" className="text-2xl tracking-tight">Home</Link>
+        //     </div>
+        //     <div className="w-full block flex-grow lg:items-end text-lg lg:w-auto">
+        //         <div className="lg:flex-grow">
+        //             <Link href="/about">About Me</Link>
+        //         </div>
+        //         <div className=" lg:flex-grow">
+        //             <Link href="/portfolio">Portfolio</Link>
+        //         </div>
+        //         <div className="lg:flex-grow">
+        //             <Link href="/contact">Contact Me</Link>
+        //         </div>
+        //     </div>
+        // </nav>
