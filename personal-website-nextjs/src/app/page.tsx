52cd3df2bd680889cfcbec34f5c36609f7b27e98
Kris French <frenchie43972@gmail.com>
2023-12-19 19:30:31 +0900

    Tidying up the home page. Ready to move on to the about page.

--- personal-website-nextjs/src/app/page.tsx
+++ personal-website-nextjs/src/app/page.tsx
@@ -1,21 +1,21 @@
 import Image from "next/image"
-import profilePic from '/public/profile_pic.jpg';
+import brandLogo from '/public/k_f_logo.png';
 
 export default function Home() {
   return (
-    <div className="container w-2/4 mx-auto p-8 flex flex-col items-center justify-center">
-      <div className="w-64 h-64 relative rounded-full overflow-hidden mb-8">
+    <div className="container w-2/4 mx-auto p-8 flex flex-col items-center ">
+      <div className="w-96 h-96 relative rounded-full overflow-hidden -mb-16">
         <Image 
-          alt="Picture of a very handsome dude" 
-          src={profilePic}
+          alt="A logo for Kris french Web Developer" 
+          src={brandLogo}
           objectFit="cover"
           layout="fill"
         />
       </div>
       <div>
-        <p className="text-4xl mt-8 mb-4 text-center"> Hello, My Name is Kris French.</p>
-        <p className="text-xl">
-          Transforming visions into reality, I develop solutions that marry beauty with efficiency.  My approach centers on sleek, user-friendly designs underpinned by robust functionality. Every project is an opportunity for me to push the boundaries of modern, accessible web design, ensuring that your online presence is nothing short of exceptional.
+        <p className="text-3xl mt-8 mb-4 text-center"> Hello, My Name is Kris French.</p>
+        <p className="text-l">
+          Transforming visions into reality, I develop solutions that marry beauty with efficiency.  My approach centers on sleek, user-friendly designs underpinned by robust functionality. Each project is a chance for me to explore new horizons in modern, accessible digital solutions, ensuring that your online and interactive presence stands out as truly exceptional.
         </p>
       </div>
     </div>
--- personal-website-nextjs/src/components/header.tsx
+++ personal-website-nextjs/src/components/header.tsx
@@ -1,12 +1,23 @@
 import Link from "next/link";
+import Image from "next/image";
+import brandLogo from '/public/k_f_logo.png';
 
 export default function Header() {
     return (
 
         <div className='w-full bg-slate-200 shadow-md z-10'>
-            <nav className='container relative flex flex-wrap items-center justify-between mx-auto p-8'>
-                <Link href='/' className='font-bold text-3xl'>Home</Link>
-                <div className='space-x-8 text-xl'>
+            <nav className='w-full relative flex flex-wrap items-center justify-between p-2'>
+                <Link href='/' className='font-bold flex flex-wrap items-center justify-between text-3xl'>
+                    <div className="w-28 h-28 m-0">
+                    <Image 
+                            src={brandLogo}
+                            alt="Photo of a very handsome gentlemen"
+                        /> 
+                    </div>
+                    
+                </Link>
+              
+                <div className='space-x-8 text-xl mr-4'>
                     <Link href='/about'>About</Link>
                     <Link href='/portfolio'>Portfolio</Link>
                     <Link href='/contact'>Contact</Link>
